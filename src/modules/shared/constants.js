28f554c2574bc6a2965b2ac49a2462b4eb775749
MK Barriault <MK.BARRIAULT@outlook.com>
2026-01-09 21:43:34 -0500

    shared constants and level data

---
+++ src/modules/shared/constants.js
@@ -0,0 +1,19 @@
+// CONSTANTS
+
+export const GROUND_Y = 440;
+export const GROUND_HEIGHT = 50;
+
+
+export const COW_WIDTH = 80;
+export const COW_HEIGHT = 80;
+export const COW_FRAME_COUNT = 9;
+export const ABDUCTION_SPEED = 150;
+export const BEAM_WIDTH = 50;
+export const BEAM_COLOR = "rgba(117, 251, 30, 0.63)";
+export const BEAM_GLOW_COLOR = "rgba(117, 251, 30, 0.2)";
+
+
+export const CAT_FRAME_COUNT = 10;
+export const MAX_TRAIL_POINTS = 120;
+export const HITBOX_WIDTH_RATIO = 0.65;
+export const HITBOX_OFFSET_X = 0.15;
---
+++ src/modules/shared/levelData.js
@@ -0,0 +1,16 @@
+// LEVEL DATA
+export const levelData = {
+  platforms: [],
+  groundSegments: [],
+  levelLength: 10000
+};
+
+export function updateLevelData(newData) {
+  Object.assign(levelData, newData);
+}
+
+export function resetLevelData() {
+  levelData.platforms = [];
+  levelData.groundSegments = [];
+  levelData.levelLength = 10000;
+}
